edf2b36e0dea865863dd24d2042f50ee47dda783
dhiaboukadida <157314812+dhiaboukadida@users.noreply.github.com>
2024-01-26 21:20:41 +0100

    Signup

---
+++ src/pages/Signup.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import './Signup.css'
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    address: '',
+    phoneNumber: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <h2>Create a new customer account</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>First Name:</label>
+          <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Last Name:</label>
+          <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Address:</label>
+          <input type="text" name="address" value={formData.address} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Phone Number:</label>
+          <input type="text" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="text" name="email" value={formData.email} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Confirm Password:</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup ;
