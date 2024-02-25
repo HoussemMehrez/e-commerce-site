266994bcf1052219d15c547aec1476405fd9d105
dhiaboukadida <157314812+dhiaboukadida@users.noreply.github.com>
2024-02-25 16:52:29 +0100

    sign

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -38,83 +38,90 @@ const Signup = () => {
         <div className='information'>
           <h3 style={{ color: 'black' }}>Personal Information:</h3>
           <div className='Name'>
-            <label htmlFor="firstName">First Name:</label>
+            <label htmlFor="firstName">First Name *</label>
             <input
               type="text"
               name="firstName"
               value={formData.firstName}
               onChange={handleChange}
               required
+              style={{ backgroundColor: '#5573FF', color: 'white', width: '504px', height: '60px', padding: '10px' }}
             />
           </div>
           <div className='Name'>
-            <label htmlFor="lastName">Last Name:</label>
+            <label htmlFor="lastName">Last Name *</label>
             <input
               type="text"
               name="lastName"
               value={formData.lastName}
               onChange={handleChange}
               required
+              style={{ backgroundColor: '#5573FF', color: 'white', width: '504px', height: '60px', padding: '10px' }}
             />
           </div>
         </div>
         <div className='information'>
           <h4 style={{ color: 'black' }}>Address Information:</h4>
           <div className='Name'>
-            <label htmlFor="address">Address:</label>
+            <label htmlFor="address">Address *</label>
             <input
               type="text"
               name="address"
               value={formData.address}
               onChange={handleChange}
               required
+              style={{ backgroundColor: '#5573FF', color: 'white', width: '504px', height: '60px', padding: '10px' }}
             />
           </div>
           <div className='Name'>
-            <label htmlFor="phoneNumber">Phone Number:</label>
+            <label htmlFor="phoneNumber">Phone Number *</label>
             <input
               type="text"
               name="phoneNumber"
               value={formData.phoneNumber}
               onChange={handleChange}
               required
+              style={{ backgroundColor: '#5573FF', color: 'white', width: '504px', height: '60px', padding: '10px' }}
             />
           </div>
         </div>
         <div className='information'>
           <h5 style={{ color: 'black' }}>Login Information:</h5>
           <div className='Name'>
-            <label htmlFor="email">Email:</label>
+            <label htmlFor="email">Email *</label>
             <input
               type="email"
               name="email"
               value={formData.email}
               onChange={handleChange}
               required
+              style={{ backgroundColor: '#5573FF', color: 'white', width: '504px', height: '60px', padding: '10px' }}
             />
           </div>
           <div className='Name'>
-            <label htmlFor="password">Password:</label>
+            <label htmlFor="password">Password *</label>
             <input
               type="password"
               name="password"
               value={formData.password}
               onChange={handleChange}
               required
+              style={{ backgroundColor: '#5573FF', color: 'white', width: '504px', height: '60px', padding: '10px' }}
             />
           </div>
           <div className='Name'>
-            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <label htmlFor="confirmPassword">Confirm Password **</label>
             <input
               type="password"
               name="confirmPassword"
               value={formData.confirmPassword}
               onChange={handleChange}
               required
+              style={{ backgroundColor: '#5573FF', color: 'white', width: '504px', height: '60px', padding: '10px' }}
             />
           </div>
         </div>
-        <button type="submit">Sign up</button>
+        <button type="submit" style={{ fontSize: '25px' }}>Sign up</button>
       </form>
     </div>
   );
